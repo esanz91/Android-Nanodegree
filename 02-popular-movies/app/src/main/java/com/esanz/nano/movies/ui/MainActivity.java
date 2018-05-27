e008713d93689c39275f46cca31ce64453484bac
Elizabeth Sanz <esanz91@gmail.com>
2018-05-26 17:30:43 -0700

    display movie posters in a grid

--- 02-popular-movies/app/src/main/java/com/esanz/nano/movies/ui/MainActivity.java
+++ 02-popular-movies/app/src/main/java/com/esanz/nano/movies/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.esanz.nano.movies.ui;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.esanz.nano.movies.R;
 import com.esanz.nano.movies.repository.MovieRepository;
@@ -10,12 +11,19 @@ import com.esanz.nano.movies.repository.api.MovieRemoteDataSource;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final MovieAdapter movieAdapter = new MovieAdapter();
+
     private MovieViewModel movieViewModel;
+
+    private RecyclerView movieView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movieView = findViewById(R.id.movies);
+        movieView.setAdapter(movieAdapter);
 
         // TODO use dagger injection
         MovieRepository movieRepository = MovieRepository.getInstance(MovieRemoteDataSource.getInstance());
@@ -25,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         movieViewModel.loadTopRatedMovies();
         movieViewModel.topRatedLiveData
                 .observe(this, (topRatedResponse) -> {
-                    // TODO show movies in RecyclerView
+                    movieAdapter.setMovies(topRatedResponse.movies);
                 });
     }
 
