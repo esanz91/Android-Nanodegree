4b63b7f2f5288bf4dbc772c3168a2de593f49254
Elizabeth Sanz <esanz91@gmail.com>
2018-09-27 23:24:57 -0700

    add backup images and use them

--- 04-baking/app/src/main/java/com/esanz/nano/ezbaking/respository/model/Recipe.java
+++ 04-baking/app/src/main/java/com/esanz/nano/ezbaking/respository/model/Recipe.java
@@ -5,12 +5,14 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.annotation.DrawableRes;
 
+import com.esanz.nano.ezbaking.R;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity(tableName = Recipe.TABLE_NAME,
         indices = {@Index(Recipe.COLUMN_PRIMARY_KEY)})
@@ -18,6 +20,17 @@ public class Recipe {
 
     public static final String TABLE_NAME = "recipes";
     public static final String COLUMN_PRIMARY_KEY = "recipe_id";
+
+    private static final String NUTELLA = "Nutella Pie";
+    private static final String BROWNIE = "Brownies";
+    private static final String YELLOW_CAKE = "Yellow Cake";
+    private static final String CHEESECAKE = "Cheesecake";
+    private static final Map<String, Integer> BACKUP_IMAGE = new HashMap<String, Integer>() {{
+        put(NUTELLA, R.drawable.nutella_pie);
+        put(BROWNIE, R.drawable.brownie);
+        put(YELLOW_CAKE, R.drawable.yellow_cake);
+        put(CHEESECAKE, R.drawable.cheesecake);
+    }};
 
     @PrimaryKey
     @ColumnInfo(name = COLUMN_PRIMARY_KEY)
@@ -48,6 +61,11 @@ public class Recipe {
             }
         }
         return -1;
+    }
+
+    @DrawableRes
+    public int getBackupImage() {
+        return BACKUP_IMAGE.containsKey(name) ? BACKUP_IMAGE.get(name) : R.drawable.recipe_placeholder;
     }
 
 }
